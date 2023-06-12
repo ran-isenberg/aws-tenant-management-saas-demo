4b353bfc9d65439b72548ab74c966f02c42f0ff2
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2023-06-12 16:10:36 +0300

    Update sqs.py

--- tenant_management_integrations/schemas/sqs.py
+++ tenant_management_integrations/schemas/sqs.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Annotated, List, Optional, Union
 
 from aws_lambda_powertools.utilities.parser.models import SqsModel, SqsRecordModel
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, conlist
 
 from tenant_management_integrations.schemas.sns import ActivateReq, CreateReq, DeleteReq, SuspendReq, UpdateReq
 
@@ -32,7 +32,7 @@ class ServiceFailure(BaseModel):
 
 # this schema is the basic response schema that each service pushes to the tenant mgmt fifo SQS
 class ServiceResponse(Ack):
-    failures: Optional[Annotated[list, Field(min_length=1)]]
+    failures: Optional[conlist(ServiceFailure, min_items=1)]
     endpoint: Optional[str]  # UI endpoint of the tenant
     api_endpoint: Optional[str]  # API endpoint of the tenant
 
